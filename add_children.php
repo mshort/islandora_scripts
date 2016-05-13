25debefff30c6386ffa19695e6b06d0e3d0a577e
Matthew Short <mshort@niu.edu>
2016-05-13 14:49:38 -0500

    Update add_children.php

--- add_children.php
+++ add_children.php
@@ -1,15 +1,9 @@
 #!/usr/bin/env drush
 
-// Adds children to compound objects after batch ingest. Updates child label and MODS. Updates parent thumbnail.
-// Requires child label with parent identifier and sequence number.
-
-// Author: Matthew Short
-// Date: 2016-05-12
-
 <?php
 
 $connection = islandora_get_tuque_connection();
-$children = $connection->repository->ri->sparqlQuery('select distinct ?pid where {?pid <fedora-rels-ext:isMemberOfCollection>+ <info:fedora/rbsc:schreiner> ; <fedora-model:hasModel> <info:fedora/islandora:sp_large_image_cmodel> .}', 'ro$
+$children = $connection->repository->ri->sparqlQuery('select distinct ?pid where {?pid <fedora-rels-ext:isMemberOfCollection> <info:fedora/rbsc:schreiner> ; <fedora-model:hasModel> <info:fedora/islandora:sp_large_image_cmodel> . OPTIONA$
 
 foreach ($children as $row){
         $pid = $row['pid']['value'];
@@ -36,22 +30,14 @@ foreach ($children as $row){
                 $parent = $connection->repository->getObject($parent);
                 $object->relationships->add(FEDORA_RELS_EXT_URI, 'isConstituentOf', $parent);
                 $object->relationships->add(ISLANDORA_RELS_EXT_URI, "isSequenceNumberOf" . str_replace(':', '_', $parent), intval($sequence), RELS_TYPE_PLAIN_LITERAL);
-                $object->relationships->remove(FEDORA_RELS_EXT_URI, 'isMemberOfCollection', 'rbsc:schreiner');
                 $parent_mods = $parent['MODS']->content;
-                $object->label = $parent->label . " (page " . intval($sequence) . ")";
+                //$parent_label = $parent->label;
+                //$object->label = $parent_label . " (page " . intval($sequence) . ")";
                 $object['MODS']->setContentFromString($parent_mods);
                 echo "Done processing parent and child.\n\n";
         } else {
                 echo "----------\nFailed to find parent for" . $label . "\n----------\n\n";
         }
-}
-
-$parents = $connection->repository->ri->sparqlQuery('select distinct ?pid where {?pid <fedora-rels-ext:isMemberOfCollection>+ <info:fedora/rbsc:schreiner> ; <fedora-model:hasModel> <info:fedora/islandora:compoundCModel> .}', 'rows');
-
-foreach ($parent as $row){
-        $pid = $row['pid']['value'];
-        $object = $connection->repository->getObject($pid);
-        islandora_compound_object_update_parent_thumbnail($object);
 }
 
 ?>
